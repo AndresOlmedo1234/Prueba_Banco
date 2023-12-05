60a2fa87caf361c8658dbcbf6ad0932b1590a71d
Roberto Abata <rabata@viamatica.com>
2023-12-05 16:47:29 -0500

    First Commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,5 @@
 import type { GatsbyConfig } from "gatsby";
+const path = require(`path`)
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -9,7 +10,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-sass", `gatsby-transformer-sharp`, `gatsby-plugin-sharp`,"gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
@@ -30,5 +31,20 @@ const config: GatsbyConfig = {
     __key: "pages"
   }]
 };
+
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
+}
 
 export default config;
